efbca12f4c843fa05ca2d0d3d278020b72eddb34
thekrin <elvineba@code.edu.az>
2023-01-18 14:08:24 +0400

    Add project files.

---
+++ TASKS 3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _3_TASKS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Verilmiş N tam ədədinin rəqəmləri cəmini tapan metod
+            //int number = 5782;
+            //int sumofDigits = 0;
+            //while (number > 0) ;
+            //{
+            //    var lastDigit = number % 10;
+            //    sumofDigits += lastDigit;
+            //    number = (number - lastDigit) / 10;
+            //}         
+            //Console.WriteLine(sumofDigits);
+
+            ////Verilmiş M dəyərindən verilmiş N dəyərinədək 21 - ə bölünən ədədlərin ədədi ortasını tapan metod
+            //int n = 50;
+            //int m = 150;
+            //double sum = 0;
+            //int count = 0;
+
+            //for (int i = n; i < m ; i++)
+            //{
+            //    if (i % 21 == 0)
+            //    {
+            //        sum += i;
+            //        count++;
+            //    }
+            //}
+            //if (count == 0)
+            //{
+            //    Console.WriteLine("21 e bolunen eeded yoxdur");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine(sum / count);
+            //}
+
+            //Verilmis yazıda a herfi olub olmadigini tapan metod
+            //Console.WriteLine("Yazini daxil edin");
+            //string word = Console.ReadLine();
+            //bool  hasWantedChar = false;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    if (word[i] == 'a')
+            //    {
+            //        hasWantedChar = true;
+
+            //    }              
+            //}  
+            // if (hasWantedChar == true)
+            //{
+            //    Console.WriteLine("Yazida a herfi var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yazida a herfi yoxdur");
+            //}  
+
+
+
+
+        }
+    }
+}
